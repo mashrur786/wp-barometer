08eeef886d4b9a11d6c931dd0a92993a224fda89
mashrur-epoch <mashrur@web-epoch.com>
2018-12-09 22:54:08 +0000

    Custom post meta data are being saved into database table WP_Postmeta table.

--- wpbarometer.php
+++ wpbarometer.php
@@ -63,6 +63,7 @@ Copyright 2018 Mashrur Chowdhury.
 		'labels' => $labels,
 		'public' => false,
 		'show_ui' => true,
+		'capability_type' => 'post',
 		'show_in_nav_menus' => true,
 		'supports' => array(
 			'title'
@@ -70,16 +71,7 @@ Copyright 2018 Mashrur Chowdhury.
 		);
 	    register_post_type( 'wpbarometer', $args );
     }
-
     add_action( 'init', 'wp_barometer_register_post_type' );
-
-    /*
-    * Setup custom meta boxes for the wp-barometer custom post type page
-    */
-    function wp_barometer_metabox_create() {
-        add_meta_box( 'wpbarometer_meta', __( 'WP Barometer Options' ), 'wpbarometer_metabox_display', 'wpbarometer' );
-    }
-    add_action( 'add_meta_boxes', 'wp_barometer_metabox_create' );
 
     /*
     * load bootstrap stylesheet and js for admin page
@@ -91,182 +83,159 @@ Copyright 2018 Mashrur Chowdhury.
     add_action( 'admin_enqueue_scripts', 'wp_barometer_admin_load_assets' );
 
 
+    /*
+    * Setup custom meta boxes for the wp-barometer custom post type page
+    */
+    function wp_barometer_metabox_create() {
+        add_meta_box(
+                'wpbarometer_meta',
+                __( 'WP Barometer Options' ),
+                'wpbarometer_metabox_display',
+                'wpbarometer'
+        );
+    }
+    add_action( 'add_meta_boxes', 'wp_barometer_metabox_create' );
+
 
     /**
     * Meta box display for the barometer post type.
     *
     * Provides the form controls necessary to select the color of the barometer as well as:
-    * Percentage input
-    * X of Y input
-    * Label Display mode (only applicable if global show percentages option is on):
-    *  - None
-    *  - Percentage only
-    *  - Text label
-    *  - Percentage (text label)
-    *  - Text label (percentage)
     */
-    function wpbarometer_metabox_display( $post ){
+    function wpbarometer_metabox_display($post){
+
+        // Nonce field to validate form request came from current site
+        wp_nonce_field(basename(__FILE__), 'wp_barometer_fields');
+        // Get the location data if it's already been entered
+        // Output the field
+        render_field($post->ID,'Target Amount', 'wp_bar_target', 'number');
+        render_field($post->ID, 'Amount Raised','wp_bar_raised', 'number');
+        render_field($post->ID, 'Width','wp_bar_width', 'number');
+        render_field($post->ID, 'Height','wp_bar_height', 'number');
+        render_field($post->ID, 'Background Color','wp_bar_bgcolor');
+        render_field($post->ID, 'Bar color','wp_bar_color');
+        render_field($post->ID, 'Meter Orientation','wp_bar_orientation', 'select', array('horizontal', 'vertical'));
+        render_field($post->ID, 'Animation Speed','wp_bar_animation_speed');
+        render_field($post->ID, 'Counter Speed','wp_bar_counter_speed');
+        render_field($post->ID, 'Display Total','wp_bar_display_total', 'checkbox');
 
 
-         $goal = '60000';
-         $raised =  '2000';
-         $orientation = 'vertical';
-         $width = '100%';
-         $bgColor = "#eed7ca";
-         $barColor = "#c37041";
-         $counterSpeed = 3000;
-         $animationSpeed = 3000;
-         $displayTotal = true;
 
-        ?>
-        <table class="table table-striped table-borderless">
-            <thead>
-                <tr>
-                    <th>Filed</th>
-                    <th>Value</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="form-group">
-                    <td>
-                        <label  for="">
-                            Target:
-                        </label>
-                    </td>
-                    <td>
-                        <input class="form-control" id="" name="__wp_barometer_target" type="text">
-                    </td>
-                </tr>
-                <tr class="form-group">
-                    <td>
-                        <label for="">
-                            Raised:
-                        </label>
-                    </td>
-                    <td>
-                        <input class="form-control" id="" name="__wp_barometer_raised" type="text">
-                    </td>
-                </tr>
-                 <tr class="form-group">
-                    <td>
-                        <label for="">
-                            Orientation:
-                        </label>
-                    </td>
-                    <td>
-                        <input class="form-control" id="" name="__wp_barometer_orientation" type="text">
-                    </td>
-                </tr>
-                <tr class="form-group">
-                    <td>
-                        <label for="">
-                            Width:
-                        </label>
-                    </td>
-                    <td>
-                        <input class="form-control" id="" name="__wp_barometer_width" type="text">
-                    </td>
-                </tr>
-                <tr class="form-group">
-                    <td>
-                        <label for="">
-                            Background Color:
-                        </label>
-                    </td>
-                    <td>
-                        <input class="form-control" id="" name="__wp_barometer_bgcolor" type="text">
-                    </td>
-                </tr>
-                <tr class="form-group">
-                    <td>
-                        <label for="">
-                            Bar Color:
-                        </label>
-                    </td>
-                    <td>
-                        <input class="form-control" id="" name="__wp_barometer_barcolor" type="text">
-                    </td>
-                </tr>
-                 <tr class="form-group">
-                    <td>
-                        <label for="">
-                            Counter Speed:
-                        </label>
-                    </td>
-                    <td>
-                        <input class="form-control" id="" name="__wp_barometer_cspeed" type="text">
-                    </td>
-                </tr>
-                 <tr class="form-group">
-                    <td>
-                        <label for="">
-                            Animation Speed:
-                        </label>
-                    </td>
-                    <td>
-                        <input class="form-control" id="" name="__wp_barometer_aspeed" type="text">
-                    </td>
-                </tr>
-                 <tr class="form-group">
-                    <td>
-                        <label for="">
-                            Display Total:
-                        </label>
-                    </td>
-                    <td>
-                        <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="__wp_barometer_display_total" value="" id="defaultCheck1">
-                        </div>
-                    </td>
-                </tr>
+    }
 
-            </tbody>
-        </table>
-    <?php
+    function render_field($id, $label ,$fieldName, $fieldType = "text", $args = []){
+        // Get the location data if it's already been entered
+        $key_value = get_post_meta($id, $fieldName, true);
+        // Output the field
+        $output = "<label>$label</label>";
+        switch ($fieldType) {
+
+            case 'number' :
+                $output .='<input type="number" name="'. $fieldName. '" value="' . $key_value . '" class="form-control">';
+                break;
+
+            case 'select' :
+                $output .= '<br>';
+                $output .= "<Select name='" .$fieldName. "' class='form-control'>";
+                foreach($args as $arg){
+                    if($key_value == $arg)
+                        $output .='<option value="' .  $arg  . '" class="form-control" selected>'. ucfirst($arg) .'</option>';
+
+                    $output .='<option value="' .  $arg  . '" class="form-control" selected>'. ucfirst($arg) .'</option>';
+                }
+                $output .= "</Select>";
+                break;
+
+            case 'checkbox' :
+
+                $output .= '<div class="form-check">';
+                $output .= '<input name="'. $fieldName .'" class="form-check-input" type="checkbox"' . ($key_value == 'on'? 'checked' : ' ') .'>';
+                $output .= '</div>';
+                break;
+
+            default:
+                $output .= '<input type="text" name="'. $fieldName. '" value="' . $key_value . '" class="form-control">';
+                break;
+          
+        }
+        
+        $output .= '<br>';
+        echo $output;
+
+    }
 
     /*
     * Saves the meta box info for the post
+     * - wp_barometer_meta_save
+     * @param Post ID
+     *
+     * @return String
     */
-    function wp_barometer_metabox_save( $post_id ) {
-        if ( isset( $_POST['tdd_pb_color'] ) )
-        update_post_meta( $post_id, '_tdd_pb_color', sanitize_html_class( $_POST['tdd_pb_color'] ) );
 
-        if ( isset( $_POST['tdd_pb_custom_color'] ) )
-        update_post_meta( $post_id, '_tdd_pb_custom_color', tdd_pb_sanitize_color_hex_raw( $_POST['tdd_pb_custom_color'] ) );
-        else
-        delete_post_meta( $post_id, '_tdd_pb_custom_color' );
+    function wp_barometer_meta_save( $post_id) {
+        //die(var_dump($_POST));
+        //check if nonce filed is set
+        if( !isset( $_POST['wp_barometer_fields'] )) return;
 
-        if ( isset( $_POST['tdd_pb_percentage'] ) ) {
-        update_post_meta( $post_id, '_tdd_pb_percentage', abs( floatval( $_POST['tdd_pb_percentage'] ) ) );
+        //skip auto save
+        if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+            return $post_id;
         }
-        if ( isset( $_POST['tdd_pb_start'] ) ) {
-        update_post_meta( $post_id, '_tdd_pb_start', floatval( $_POST['tdd_pb_start'] ) );
+
+        // If this isn't a 'wpbarometer' post, don't update it.
+        $post_type = get_post_type($post_id);
+        if ( "wpbarometer" != $post_type ) return;
+
+        $meta_keys = [
+            'wp_bar_target',
+            'wp_bar_raised',
+            'wp_bar_width',
+            'wp_bar_height',
+            'wp_bar_bgcolor',
+            'wp_bar_color',
+            'wp_bar_orientation',
+            'wp_bar_animation_speed',
+            'wp_bar_counter_speed',
+            'wp_bar_display_total',
+            ];
+
+        //die(var_dump($_POST));
+        foreach($_POST as $key => $value){
+            if(in_array($key, $meta_keys)) {
+                 if (isset($_POST[$key])) {
+                     update_post_meta($post_id, $key, sanitize_text_field($value));
+                 }
+            }
+
         }
-        if ( isset( $_POST['tdd_pb_end'] ) ) {
-        update_post_meta( $post_id, '_tdd_pb_end', floatval( $_POST['tdd_pb_end'] ) );
-        }
-        if ( isset( $_POST['tdd_pb_input_method'] ) ){
-        switch ( $_POST['tdd_pb_input_method'] ){
-            case 'xofy':
-                update_post_meta( $post_id, '_tdd_pb_input_method', 'xofy' );
-                break;
-            default:
-                update_post_meta( $post_id, '_tdd_pb_input_method',  'percentage' );
-        }
-        }
-        if ( isset( $_POST['tdd_pb_percentage_display'] ) ){
-        update_post_meta( $post_id, '_tdd_pb_percentage_display', 'on' );
-        } else {
-        update_post_meta( $post_id, '_tdd_pb_percentage_display', 'off' );
-        }
-        if ( isset( $_POST['tdd_pb_xofy_display'] ) ){
-        update_post_meta( $post_id, '_tdd_pb_xofy_display', 'on' );
-        } else {
-        update_post_meta( $post_id, '_tdd_pb_xofy_display', 'off' );
-        }
+
     }
-    add_action( 'save_post', 'tdd_pb_metabox_save' );
-}
+    add_action( 'save_post', 'wp_barometer_meta_save', 10, 3);
+
+    /**
+    * Register all shortcodes
+    *
+    * @return null
+    */
+    function register_wp_barometer_shortcode() {
+        add_shortcode( 'wpbarometer', 'wp_barometer_shortcode' );
+    }
+    add_action( 'init', 'register_wp_barometer_shortcode' );
+
+    /**
+    * WP Barometer Shortcode Callback
+    * - WP Barometer
+    *
+    * @param Array $atts
+    *
+    * @return string
+    */
+    function wp_barometer_shortcode($args) {
+
+
+       echo 'hi786';
+
+    }
 
 
 
